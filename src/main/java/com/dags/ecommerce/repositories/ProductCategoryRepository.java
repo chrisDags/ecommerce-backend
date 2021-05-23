1b9b053b349d54f80bd5d5f11005b2a3fb1ada63
Chris <christopher-dags@hotmail.com>
2021-05-23 14:52:22 -0400

    Added findByCategoryId method in ProductRepository. Spring Data Rest auto-magically exposes the endpoint: localhost:8080/api/products/search/findByCategoryId?id=#

--- src/main/java/com/dags/ecommerce/repositories/ProductCategoryRepository.java
+++ src/main/java/com/dags/ecommerce/repositories/ProductCategoryRepository.java
@@ -2,6 +2,8 @@ package com.dags.ecommerce.repositories;
 
 import com.dags.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
